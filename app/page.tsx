805aa56a7d00c14558c85f2a237c627029fd5997
AviSelvakumar <aviselvakumar@icloud.com>
2026-05-25 13:52:00 -0400

    feat: enhance prompt section layout and improve styling

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
               <Title level={2} style={{ marginBottom: 0, fontWeight: 900 }} className='section-content'>The Prompt</Title>
               <Text className='prompt'>Invent entirely new ways of creating art that wouldn't exist without technology.</Text>
             </Flex>
-  
+          <Flex vertical gap={16} align="center" justify="center" className='prompt-container'>
           <Text className='prompt-desc'>Think beyond just drawing apps. What if you could create art that responds to sound, moves with the wind, or tells a story through interaction? Some ideas: </Text>
           <ul className='prompt-ideas'>
             <li>A canvas that gets painted by sound</li>
@@ -58,6 +58,7 @@ export default function Home() {
             <li>A visualization of your daily life turned into abstract art</li>
           </ul>
           <Text className='prompt-desc'>The possibilities are endless! We encourage you to think outside the box and create something truly unique.</Text>
+        </Flex>
         </Flex>
         {/*Section 3 - Register*/}
         <Flex vertical gap={16} align="center" justify="center" style={{ height: '100vh' }} id='register' className='section register'>
